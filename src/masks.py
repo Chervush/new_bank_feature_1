6b6632ca74ddc607436bcb83c40f74ecd2e8854a
chervush <achervov0042@gmail.com>
2025-06-30 00:01:11 +0300

    все файлы проверены линтерами и приведены к pep8

--- src/masks.py
+++ src/masks.py
@@ -1,14 +1,14 @@
 from typing import Union
 
 
-def get_mask_card_number(card_number: Union[int, str]) -> str:
+def get_mask_card_number(card_number: Union[str]) -> str:
     """Функция маскировки номера банковской карты"""
 
     masked_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
     return masked_number
 
 
-def get_mask_account(account_number: Union[int, str]) -> str:
+def get_mask_account(account_number: Union[str]) -> str:
     """Функция маскировки номера банковского счета"""
 
     masked_number = "**" + account_number[-4:]
--- src/widget.py
+++ src/widget.py
@@ -1,37 +1,26 @@
 import re
-from src.masks import get_mask_card_number
-from src.masks import get_mask_account
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(acc_card_number: str) -> str:
     """Принимает название карты или счета и его номер и возвращает то же самое,
     но с маскированным номером"""
 
-    just_numbers = re.sub(r"[^0-9]", '', acc_card_number)
-    just_words = re.sub(r"[0-9 ]", '', acc_card_number)
+    just_numbers = re.sub(r"[^0-9]", "", acc_card_number)
+    just_words = re.sub(r"[0-9 ]", "", acc_card_number)
     if len(just_numbers) == 16:
         masked_number = get_mask_card_number(just_numbers)
     elif len(just_numbers) == 20:
         masked_number = get_mask_account(just_numbers)
     else:
-        return 'Это не номер карты или счета.'
+        return "Это не номер карты или счета."
 
-
-    masked_all = just_words + ' ' + masked_number
+    masked_all = just_words + " " + masked_number
     return masked_all
 
 
 def get_date(date: str) -> str:
     """приводит строку с датой к формату 'ДД.ММ.ГГГГ'"""
-    formatted_date = date[8:10] + '.' + date[5:7] + '.' + date[:4]
+    formatted_date = date[8:10] + "." + date[5:7] + "." + date[:4]
     return formatted_date
-
-
-#date = "2024-03-11T02:26:18.671407"
-#print(get_date(date))
-#card_acc = 'Maestro 1596837868705199'
-#print(mask_account_card(card_acc))
-
-
-
-
