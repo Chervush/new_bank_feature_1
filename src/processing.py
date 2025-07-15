542475e26d9d14eabaf6894cd78227790fbfdff5
chervush <achervov0042@gmail.com>
2025-07-16 01:19:51 +0300

    Апдейт домашки 10.1

--- src/processing.py
+++ src/processing.py
@@ -1,35 +1,40 @@
 # тестовый словарь для проверки
+from datetime import datetime
+
 list_of_dates = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
 
-def filter_by_state(list_of_dates: list, state: str = 'EXECUTED') -> list:
-
-    '''Функция принимает список словарей и возвращает новый список словарей, содержащий только те словари,
+def filter_by_state(list_of_dates: list[dict[str, object]], state: str = "EXECUTED") -> list:
+    """Функция принимает список словарей и возвращает новый список словарей, содержащий только те словари,
     у которых ключ state
-    соответствует указанному значению.'''
+    соответствует указанному значению: "EXECUTED" или "CANCELED"."""
 
     filtered = []
     for entry in list_of_dates:
-        if entry.get('state') == state:
+        if entry.get("state") == state:
             filtered.append(entry)
     return filtered
 
 
-def sort_by_date(list_of_dates: list, reversed: bool = True) -> list:
-    ''''Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
+def sort_by_date(list_of_dates: list[dict[str, object]], is_reversed: bool = True) -> list:
+    """'Функция принимает список словарей и необязательный параметр is_reversed, задающий порядок сортировки
     (по умолчанию — убывание).
-    Функция возвращает новый список словарей, отсортированный по дате'''
+    Функция возвращает новый список словарей, отсортированный по дате"""
 
-    sorted_list = sorted(list_of_dates, key=lambda entry: (entry['date'])[-6:], reverse=reversed)
+    sorted_list = sorted(
+        list_of_dates,
+        key=lambda entry: datetime.fromisoformat(str(entry["date"])),
+        reverse=is_reversed
+    )
     return sorted_list
+
 
 # Проверка работы функций
 
-
 print(filter_by_state(list_of_dates))
-print(sort_by_date(list_of_dates))
+print(sort_by_date(list_of_dates, is_reversed=True))
