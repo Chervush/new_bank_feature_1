58ab2d7b69be698794dcbf8f3072b2722eb92c65
chervush <achervov0042@gmail.com>
2025-07-18 00:29:24 +0300

    Внесены все правки по домашке 10.1

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,4 @@
-# тестовый словарь для проверки
-from datetime import datetime
-
-list_of_dates = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-
-def filter_by_state(list_of_dates: list[dict[str, object]], state: str = "EXECUTED") -> list:
+def filter_by_state(list_of_dates: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция принимает список словарей и возвращает новый список словарей, содержащий только те словари,
     у которых ключ state
     соответствует указанному значению: "EXECUTED" или "CANCELED"."""
@@ -21,20 +10,14 @@ def filter_by_state(list_of_dates: list[dict[str, object]], state: str = "EXECUT
     return filtered
 
 
-def sort_by_date(list_of_dates: list[dict[str, object]], is_reversed: bool = True) -> list:
+def sort_by_date(list_of_dates: list[dict], is_reversed: bool = True) -> list[dict]:
     """'Функция принимает список словарей и необязательный параметр is_reversed, задающий порядок сортировки
     (по умолчанию — убывание).
     Функция возвращает новый список словарей, отсортированный по дате"""
 
     sorted_list = sorted(
         list_of_dates,
-        key=lambda entry: datetime.fromisoformat(str(entry["date"])),
+        key=lambda entry: datetime.fromisoformat(entry["date"]),
         reverse=is_reversed
     )
     return sorted_list
-
-
-# Проверка работы функций
-
-print(filter_by_state(list_of_dates))
-print(sort_by_date(list_of_dates, is_reversed=True))
